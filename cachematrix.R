16c7c478a84c02de4a24bd5b41b40356170cc9b6
andygoode75 <andygoode75@users.noreply.github.com>
2014-10-20 17:00:34 +0100

    Updated cachematrix.R file to complete functions
    Create a series of functions to create a special matrix and return an inverse of that matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,44 +1,44 @@
-## Create a matrix - ensure that there are default values generated 
-## create a matrix from x and assign it to m
-## assign makeCacheMatrix to cache and create functions to create inverse and retieve the inverse of m
+## Create a makeCacheMatrix function
 
 makeCacheMatrix <- function(x = matrix()) {
+##ensure that there are default values generated
   m<-NULL
-  setmatrix <- function(y){
-    x<<- y
-    m<<-NULL
-    }
-  getmatrix <-function () x
-  setinverse <- function (inverse) m <<-solve
-  getinverse <- function () m
+## create a matrix from x and assign it to m
+setmatrix <- function(y){
+  x<<- y
+  m<<-NULL
+  }
+## assign makeCacheMatrix to cache and create functions to create inverse and retieve the inverse of m
+getmatrix <-function () x
+setinverse <- function (inverse) m <<-solve
+getinverse <- function () m
   
-  list(setmatrix = setmatrix,
-       getmatrix = getmatrix,
-       setinverse = setinverse,
-       getinverse = getinverse
-       )
+list(setmatrix = setmatrix,
+  getmatrix = getmatrix,
+  setinverse = setinverse,
+  getinverse = getinverse
+   )
 }  
 
 
 ## Check if there is an existing inverse matrix, if so return existing vaue of m
-## otherwise take value held in getmatrix and inverse it
-## store result in setinverse
-## print inverse (m) to console
 
 cacheSolve <- function(x, ...) {
   m<-x$getinverse()
-  if(!is.null(m)) {
-    return (m)
+
+if(!is.null(m)) {
+  return (m)
   }
-  
+## otherwise take value held in getmatrix and inverse it
+data <- x$getmatrix()
+m<-solve(data, ...)  
+
 ## Return a matrix that is the inverse of 'x'
+## store result in setinverse
+x$setinverse(m)
 
-  data <- x$getmatrix()
-  m<-solve(data, ...)
-  x$setinverse(m)
-  m
+## print inverse (m) to console
+m
 
-  
-        
 }
 
